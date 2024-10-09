f06c254923de487101f766222d1ec82354e62cdf
jhrchicken <122204170+jhrchicken@users.noreply.github.com>
2024-10-09 14:49:19 +0900

    ㅇ

--- lib/theme/colors.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter/material.dart';
-
-const pointColor1 = Color(0xffffce32);
-const pointColor2 = Color(0xff005ad5);
-const gray400 = Color(0xff999999);
-const gray500 = Color(0xff666666);
-const gray600 = Color(0xff292929);
-const border = Color(0xffdddddd);
