93ee4aedbf8a64555cdd00698e14b4b90133e9ee
jhrchicken <122204170+jhrchicken@users.noreply.github.com>
2024-10-15 00:33:13 +0900

    Feat: 병원 모든 진료시간 표시

--- lib/widgets/hospital/hospital_hours_widget.dart
+++ lib/widgets/hospital/hospital_hours_widget.dart
@@ -2,6 +2,7 @@ import 'package:doctorviewapp/models/hospital.dart';
 import 'package:doctorviewapp/models/hours.dart';
 import 'package:doctorviewapp/providers/hospital_provider.dart';
 import 'package:doctorviewapp/providers/hours_provider.dart';
+import 'package:doctorviewapp/theme/colors.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
@@ -18,6 +19,16 @@ class HospitalHoursWidget extends StatefulWidget {
 }
 
 class _HospitalHoursWidgetState extends State<HospitalHoursWidget> {
+
+  final List<String> weekdays = [
+    '월요일',
+    '화요일',
+    '수요일',
+    '목요일',
+    '금요일',
+    '토요일',
+    '일요일',
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -42,15 +53,76 @@ class _HospitalHoursWidgetState extends State<HospitalHoursWidget> {
         const SizedBox(
           height: 20,
         ),
-        // 지도
+        // 진료시간
         Container(
-          height: 100,
           width: double.infinity,
           decoration: BoxDecoration(
-            color: Colors.grey[200],
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(8),
             border: Border.all(
-              color: Colors.grey,
+              color: Colors.grey[300] ?? Colors.black,
               width: 1,
+            ),
+          ),
+          child: Padding(
+            padding: const EdgeInsets.only(
+              top: 20,
+              left: 20,
+              right: 20,
+              bottom: 10,
+            ),
+            child: ListView.builder(
+              shrinkWrap: true,
+              physics: const NeverScrollableScrollPhysics(),
+              itemCount: hoursList.length,
+              itemBuilder: (context, index) {
+                final hours = hoursList[index];
+                return Column(
+                  children: [
+                    Row(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          hours.week,
+                          style: TextStyle(
+                            fontWeight: FontWeight.w700,
+                            color: hours.week == weekdays[DateTime.now().weekday] ? pointColor2 : Colors.grey[700],
+                          ),
+                        ),
+                        const SizedBox(
+                          width: 10,
+                        ),
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              '${hours.startTime} - ${hours.endTime}',
+                              style: TextStyle(
+                                color: hours.week == weekdays[DateTime.now().weekday] ? pointColor2 : Colors.grey[700],
+                              ),
+                            ),
+                            Text(
+                              '${hours.startBreak} - ${hours.endBreak} 휴게시간',
+                              style: TextStyle(
+                                color: hours.week == weekdays[DateTime.now().weekday] ? pointColor2 : Colors.grey[700],
+                              ),
+                            ),
+                            Text(
+                              '${hours.deadLine} 접수마감',
+                              style: TextStyle(
+                                color: hours.week == weekdays[DateTime.now().weekday] ? pointColor2 : Colors.grey[700],
+                              ),
+                            ),
+                          ]
+                        ),
+                      ],
+                    ),
+                    const SizedBox(
+                      height: 10,
+                    ),
+                  ],
+                );
+              },
             ),
           ),
         ),
