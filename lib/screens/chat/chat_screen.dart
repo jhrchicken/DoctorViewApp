3dbf7b23e36f7175be62279b7657419f662bd588
jhrchicken <122204170+jhrchicken@users.noreply.github.com>
2024-10-15 03:14:14 +0900

    Fix: 채팅 스크롤

--- lib/screens/chat/chat_screen.dart
+++ lib/screens/chat/chat_screen.dart
@@ -22,6 +22,7 @@ class _ChatScreenState extends State<ChatScreen> {
     String minute = time.split(':')[1];
 
     return Scaffold(
+      resizeToAvoidBottomInset: true,
       // 상단바
       appBar: AppBar(
         backgroundColor: Colors.white,
@@ -31,131 +32,139 @@ class _ChatScreenState extends State<ChatScreen> {
         ),
       ),
       
-      body: SingleChildScrollView(
-        child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 10),
-          child: Column(
-            children: [
-              // 날짜
-              const DateLine(),
-              const SizedBox(height: 10),
-          
-              // 보낸 메세지
-              Align(
-                alignment: Alignment.centerRight,
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.end,
-                  crossAxisAlignment: CrossAxisAlignment.end,
-                  children: [
-                    Container(
-                      margin: const EdgeInsets.only(bottom: 8),
-                      child: Text(
-                        '$hour:$minute',
-                        style: const TextStyle(fontSize: 10),
-                      ),
+      body: Column(
+        children: [
+          Expanded(
+            child: SingleChildScrollView(
+              child: Column(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 10),
+                    child: Column(
+                      children: [
+                        // 날짜
+                        const DateLine(),
+                        const SizedBox(height: 10),
+                    
+                        // 보낸 메세지
+                        Align(
+                          alignment: Alignment.centerRight,
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.end,
+                            crossAxisAlignment: CrossAxisAlignment.end,
+                            children: [
+                              Container(
+                                margin: const EdgeInsets.only(bottom: 8),
+                                child: Text(
+                                  '$hour:$minute',
+                                  style: const TextStyle(fontSize: 10),
+                                ),
+                              ),
+                              Container(
+                                constraints: const BoxConstraints(maxWidth: 280),
+                                margin: const EdgeInsets.all(5),
+                                padding: const EdgeInsets.symmetric(vertical: 8, horizontal: 10),
+                                decoration: BoxDecoration(
+                                  color: Colors.grey[300],
+                                  borderRadius: BorderRadius.circular(15),
+                                ),
+                                child: const Text(
+                                  '안녕하세요안녕하세요안녕하세요안녕하세요안녕하세요안녕하세요안녕하세요안녕하세요안녕하세요안녕하세요안녕하세요안녕하세요안녕하세요안녕하세요',
+                                  softWrap: true,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                    
+                        // 받은 메세지
+                        Align(
+                          alignment: Alignment.centerLeft,
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.start,
+                            crossAxisAlignment: CrossAxisAlignment.end,
+                            children: [
+                              const Icon(
+                                Icons.account_circle,
+                                size: 50,
+                              ),
+                              Container(
+                                constraints: const BoxConstraints(maxWidth: 280),
+                                margin: const EdgeInsets.all(5),
+                                padding: const EdgeInsets.symmetric(vertical: 8, horizontal: 10),
+                                decoration: BoxDecoration(
+                                  color: Colors.white,
+                                  border: Border.all(color: border),
+                                  borderRadius: BorderRadius.circular(15),
+                                ),
+                                child: const Text(
+                                  '누구세요',
+                                  softWrap: true,
+                                ),
+                              ),
+                              Container(
+                                margin: const EdgeInsets.only(bottom: 8),
+                                child: Text(
+                                  '$hour:$minute',
+                                  style: const TextStyle(fontSize: 10),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ],
                     ),
-                    Container(
-                      constraints: const BoxConstraints(maxWidth: 280),
-                      margin: const EdgeInsets.all(5),
-                      padding: const EdgeInsets.symmetric(vertical: 8, horizontal: 10),
-                      decoration: BoxDecoration(
-                        color: Colors.grey[300],
-                        borderRadius: BorderRadius.circular(15),
-                      ),
-                      child: const Text(
-                        '안녕하세요안녕하세요안녕하세요안녕하세요안녕하세요안녕하세요안녕하세요안녕하세요안녕하세요안녕하세요안녕하세요안녕하세요안녕하세요안녕하세요',
-                        softWrap: true,
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-          
-              // 받은 메세지
-              Align(
-                alignment: Alignment.centerLeft,
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  crossAxisAlignment: CrossAxisAlignment.end,
-                  children: [
-                    const Icon(
-                      Icons.account_circle,
-                      size: 50,
-                    ),
-                    Container(
-                      constraints: const BoxConstraints(maxWidth: 280),
-                      margin: const EdgeInsets.all(5),
-                      padding: const EdgeInsets.symmetric(vertical: 8, horizontal: 10),
-                      decoration: BoxDecoration(
-                        color: Colors.white,
-                        border: Border.all(color: border),
-                        borderRadius: BorderRadius.circular(15),
-                      ),
-                      child: const Text(
-                        '누구세요',
-                        softWrap: true,
-                      ),
-                    ),
-                    Container(
-                      margin: const EdgeInsets.only(bottom: 8),
-                      child: Text(
-                        '$hour:$minute',
-                        style: const TextStyle(fontSize: 10),
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-            ],
-          ),
-        ),
-      ),
-
-      // 입력창
-      bottomNavigationBar: Padding(
-        padding: const EdgeInsets.symmetric(
-          horizontal: 10, vertical: 10
-        ),
-        child: Row(
-          children: [
-            Expanded(
-              child: TextField(
-                controller: _messageController,
-                style: TextStyle(
-                  color: Colors.grey[900],
-                  fontSize: 14,
-                ),
-                decoration: InputDecoration(
-                  hintText: '메세지를 입력하세요',
-                  hintStyle: TextStyle(
-                    color: Colors.grey[500],
-                    fontSize: 14,
                   ),
-                  filled: true,
-                  fillColor: Colors.grey[100],
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(8),
-                    borderSide: BorderSide.none,
-                  ),
-                  contentPadding: const EdgeInsets.symmetric(
-                    horizontal: 16,
-                    vertical: 5
-                  ),
-                  // 아이콘
-                  suffixIcon: IconButton(
-                    icon: const Icon(
-                      Icons.send_rounded,
-                      color: pointColor2,
-                    ),
-                    onPressed: () {
-                      /* 전송 */
-                    },
-                  ),
-                ),
+                ],
               ),
             ),
-          ],
-        ),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(
+              horizontal: 10, vertical: 10
+            ),
+            child: Row(
+              children: [
+                Expanded(
+                  child: TextField(
+                    controller: _messageController,
+                    style: TextStyle(
+                      color: Colors.grey[900],
+                      fontSize: 14,
+                    ),
+                    decoration: InputDecoration(
+                      hintText: '메세지를 입력하세요',
+                      hintStyle: TextStyle(
+                        color: Colors.grey[500],
+                        fontSize: 14,
+                      ),
+                      filled: true,
+                      fillColor: Colors.grey[100],
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(8),
+                        borderSide: BorderSide.none,
+                      ),
+                      contentPadding: const EdgeInsets.symmetric(
+                        horizontal: 16,
+                        vertical: 5
+                      ),
+                      // 아이콘
+                      suffixIcon: IconButton(
+                        icon: const Icon(
+                          Icons.send_rounded,
+                          color: pointColor2,
+                        ),
+                        onPressed: () {
+                          /* 전송 */
+                        },
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
